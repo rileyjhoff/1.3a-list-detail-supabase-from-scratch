86dbd69f9706de6edb11925be94c17ed0e83c27f
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-23 07:41:25 -0700

    wrote renderPlayerCard function to render a player card for each avs player on the list page

---
+++ render-utils.js
@@ -0,0 +1,22 @@
+{/* <a href=""><div class="player-card">
+<h4>Nathan MacKinnon #29</h4>
+<img src="https://zhmowgcybteqgiwwrxln.supabase.in/storage/v1/object/sign/headshots/Mac.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJoZWFkc2hvdHMvTWFjLmpwZWciLCJpYXQiOjE2NDgwMDQ1MzYsImV4cCI6MTk2MzM2NDUzNn0.xXbc8kjOdOeJALgDhcO-bdo4rMOh0nQZyZl5ciiPaUg" alt="">
+</div></a> */}
+
+export function renderPlayerCard(player){
+    const anchor = document.createElement('a');
+    const playerCard = document.createElement('div');
+    const nameEl = document.createElement('h4');
+    const imgEl = document.createElement('img');
+
+    anchor.classList.add('hover');
+    playerCard.classList.add('player-card');
+
+    nameEl.textContent = player.name + ' #' + player.number;
+    imgEl.src = player.headshot;
+
+    playerCard.append(nameEl, imgEl);
+    anchor.append(playerCard);
+
+    return anchor;
+}
